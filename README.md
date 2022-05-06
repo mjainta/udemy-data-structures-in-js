cde118cf6a0b1f4ab0cf12ea665a9779762c7b66
Martin Jainta <martin.jainta@tradebyte.com>
2022-05-06 08:54:19 +0200

    1.3 The Prototype Object

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Udemy course - Data structures in JS
 
 Code done with the Udemy course [learning-data-structures-in-javascript-from-scratch](https://www.udemy.com/course/learning-data-structures-in-javascript-from-scratch)
+
+## Learnings
+
+1.3 The Prototype Object
+
+You can add class functions after defining a constructor method using prototype.
+
+```javascript
+User.prototype.getEmailAddress = function() {
+  return this.firstName + this.lastName + this.emailDomain
+}
+console.log(user1.getEmailAddress())
+console.log(user200.getEmailAddress())
+```
--- src/constructor_functions.js
+++ src/constructor_functions.js
@@ -7,11 +7,20 @@ function User(firstName, lastName, age, gender) {
 };
 
 var user1 = new User('John', 'Smith', 26, 'male');
-
 console.log(user1.firstName)
 console.log(user1)
 
 var user200 = new User('Jill', 'Robinson', 25, 'female');
-
 console.log(user200)
 console.log(user1)
+
+User.prototype.emailDomain = '@facebook.com'
+console.log(user1) // Does not show the emailDomain
+console.log(user1.emailDomain) // Shows the emailDomain
+
+User.prototype.getEmailAddress = function() {
+  return this.firstName + this.lastName + this.emailDomain
+}
+console.log(user1.getEmailAddress())
+console.log(user200.getEmailAddress())
+
