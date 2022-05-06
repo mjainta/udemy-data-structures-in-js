75d20d207779d58626f4da64310f6f73eb0a4a11
Martin Jainta <martin.jainta@tradebyte.com>
2022-05-06 09:16:03 +0200

    7. Add To Head - part 2

--- src/linked_list.js
+++ src/linked_list.js
@@ -13,5 +13,20 @@ function Node(value, next, prev) {
 
 LinkedList.prototype.addToHead = function(value) {
   let newNode = new Node(value, this.head, null);
+
+  if (this.head) {
+    this.head.prev = newNode;
+  } else {
+    this.tail = newNode;
+  }
+
+  this.head = newNode;
 };
 
+let myList = new LinkedList()
+myList.addToHead('value 1')
+console.log(myList)
+myList.addToHead('value 2')
+console.log(myList)
+myList.addToHead('value 3')
+console.log(myList)
