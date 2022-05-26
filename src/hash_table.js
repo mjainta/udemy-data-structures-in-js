a9ae06a26c68ed215ffa4bd9004b88e72d5a6edb
Martin Jainta <martin.jainta@gmail.com>
2022-05-26 14:21:01 +0200

    47. Refactoring Insert Method

--- src/hash_table.js
+++ src/hash_table.js
@@ -23,9 +23,16 @@ HashTable.prototype.hash = function(key) {
 HashTable.prototype.insert = function(key, value) {
     const index = this.hash(key);
     if (!this.buckets[index]) this.buckets[index] = new HashNode(key, value);
+    else if (this.buckets[index].key === key) {
+        this.buckets[index].value = value;
+    }
     else {
         let currentNode = this.buckets[index];
         while (currentNode.next) {
+            if (currentNode.next.key === key) {
+                currentNode.next.value = value;
+                return;
+            }
             currentNode = currentNode.next
         }
         currentNode.next = new HashNode(key, value);
@@ -37,6 +44,7 @@ const myHT = new HashTable(30);
 myHT.insert('Dean', 'dean@gmail.com');
 myHT.insert('Megan', 'megan@gmail.com');
 myHT.insert('Dane', 'dane@yahoo.com');
+myHT.insert('Dean', 'dean@yahoo.com');
 
 
 console.log(myHT.buckets);
