89db4be356abf196dcdead29bddb11f5e053c991
Martin Jainta <martin.jainta@gmail.com>
2022-05-26 10:05:01 +0200

    25. Testing Insert

--- src/binary_search_tree.js
+++ src/binary_search_tree.js
@@ -24,7 +24,18 @@ BST.prototype.insert = function(value) {
 }
 
 let bst = new BST(50);
+
 bst.insert(30)
 bst.insert(70)
+bst.insert(100)
+bst.insert(60)
+bst.insert(59)
 bst.insert(20)
-console.log(bst)
+bst.insert(45)
+bst.insert(35)
+bst.insert(85)
+bst.insert(105)
+bst.insert(10)
+
+console.log(bst.right.left.left)
+console.log(bst.right.right)
